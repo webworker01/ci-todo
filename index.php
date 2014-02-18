a917c325daccc76487a42999ee2c6e5d2e2d537d
webworker <webworker@live.com>
2014-02-17 21:58:09 -0500

    * Set development error logging to output to browser

--- index.php
+++ index.php
@@ -33,16 +33,17 @@ if (defined('ENVIRONMENT'))
 	switch (ENVIRONMENT)
 	{
 		case 'development':
-			error_reporting(E_ALL);
+                    error_reporting(E_ALL);
+                    ini_set('display_errors', 'on');
 		break;
 	
 		case 'testing':
 		case 'production':
-			error_reporting(0);
+                    error_reporting(0);
 		break;
 
 		default:
-			exit('The application environment is not set correctly.');
+                    exit('The application environment is not set correctly.');
 	}
 }
 
